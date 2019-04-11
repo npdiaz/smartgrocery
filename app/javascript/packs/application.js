dbe1306555d641d8bdf52e286e22d16367ea2a73
Nerea Pascual Diaz <nereapasc@gmail.com>
2019-04-11 13:14:15 +0200

    bought works on show now, stuck with recipes find

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,8 @@
 import "bootstrap";
-// import { searchRecipes } from './recipes_index';
-// import{ markItemAsBought } from './box';
+ import { searchRecipes } from './recipes_index';
+ import{ markItemAsBought } from './box';
 
-// const searchRecipes = document.querySelector('#results')
-// if (searchRecipes) {
-//   searchRecipes();
-// }
 
-// const markItemAsBought = document.querySelector('.box')
-// if (markItemAsBought) {
-//   markItemAsBought();
-// }
+   // searchRecipes();
+   markItemAsBought();
+
--- app/javascript/packs/box.js
+++ app/javascript/packs/box.js
@@ -1,9 +1,9 @@
 function markItemAsBought() {
   const box = document.querySelector(".box");
   box.addEventListener('click' , (event)=> {
-    const item = document.querySelector(".item-tobuy");
-    const box = document.querySelector(".fa-square")
-    item.classList.add('item-tobuy', 'item-bought');
+    const item = document.getElementById("tobuy");
+    const box = document.getElementById("square")
+    item.classList.add('item-bought');
     box.classList.replace('fa-square','fa-check-square');
     }
   )
